b515097b0f943fd9f25e9660e89a5014dc75b4ed
Juan Manuel Mougan <juan.mougan@roche.com>
2020-06-23 22:52:33 -0300

    Deleted this for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,6 @@
         <h2 class="subtitle">
           This showcases both public and private views
         </h2>
-        <div class="button-block">
-          <button v-if="!$auth.isAuthenticated" @click="login" class="button is-xl is-dark">Sign Up to Browse Events</button>
-          <h3 v-if="$auth.isAuthenticated" class="is-size-3 has-background-dark welcome">Welcome, {{ $auth.user.name }}!</h3>
-        </div>
       </div>
     </div>
   </section>
